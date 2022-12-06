5f37c19adc28dc188ebc4d0e138d7b2b64eafc48
SampadaPimparwar <sampimparwar5399@gmail.com>
2022-12-06 15:31:58 +0530

    setters and getters by sam

--- Technosignia/src/main/java/com/example/Technosignia/entity/Teacher.java
+++ Technosignia/src/main/java/com/example/Technosignia/entity/Teacher.java
@@ -14,4 +14,38 @@ public class Teacher {
 	String address;
 	
 	String contact;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	
+	
 }
